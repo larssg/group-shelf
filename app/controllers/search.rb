fa1a0156c62b9ea3694180ba415bacc77a3cabb9
Lars G. Sehested <lgs@lenio.dk>
2008-12-07 17:38:00 +0100

    Search in descriptions as well

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -1,7 +1,7 @@
 class Search < Application
   def index(q = '')
     @authors = Author.all(:name.like => "%#{q}%")
-    @items = Item.all(:title.like => "%#{q}%")
+    @items = Item.all(:conditions => ["title LIKE ? OR description LIKE ?", "%#{q}%", "%#{q}%"])
     render
   end
 end
